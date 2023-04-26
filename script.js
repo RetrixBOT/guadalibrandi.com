5ae99ce4390267983724ab7821b134dd689b37f3
RetrixUY <RetrixUY@gmail.com>
2023-04-25 23:33:19 -0300

    new flow

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const mensajeElement1 = document.getElementById("mensaje1");
 const mensajeElement2 = document.getElementById("mensaje2");
 const mensajeElement3 = document.getElementById("mensaje3");
 const contadorElement = document.getElementById("contador-regresivo");
-
+const imagen = document.getElementById('imagen');
+const title = document.getElementById('title');
 const passwordContainer = document.getElementById("password-container");
 const mensajeContainer = document.getElementById("mensaje-container");
 const passwordForm = document.getElementById("password-form");
 const passwordInput = document.getElementById("password");
+let blurValue = 300; // Valor inicial de desenfoque
 
 let index = 0;
 let element = 1;
@@ -31,14 +33,28 @@ function escribirMensaje() {
   }
 }
 
+function showImage(){
+  mensajeElement1.style.display = 'none'
+  mensajeElement2.style.display = 'none'
+  title.style.display = 'none'
+  //mensajeElement3.style.display = 'none'
+  contadorElement.style.display = 'none'
+  if (blurValue > 0) { // Si llegamos a 0, detenemos el intervalo
+    blurValue--; // Disminuimos el valor de desenfoque
+    imagen.style.filter = `blur(${blurValue}px)`; // Aplicamos el nuevo valor de desenfoque
+    setTimeout(showImage, 20);
+  }
+}
+
 function contadorRegresivo(segundos) {
-  contadorElement.innerHTML = `Redireccionando en ${segundos} segundos...`;
+  contadorElement.innerHTML = `Revelando en ${segundos} segundos...`;
   if (segundos > 0) {
     setTimeout(function () {
       contadorRegresivo(segundos - 1);
     }, 1000);
   } else {
-    window.location.assign("https://articulo.mercadolibre.com.ar/MLA-912293480-camara-instantanea-instax-mini-11-rosa-4-rollos-entrega-_JM");
+    showImage();
+    //window.location.assign("https://articulo.mercadolibre.com.ar/MLA-912293480-camara-instantanea-instax-mini-11-rosa-4-rollos-entrega-_JM");
   }
 }
 
